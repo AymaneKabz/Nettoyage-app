7a2d9eaa66445c5655d40e4dfe8646255353d7d0
Aymane Kabz <31325197+AymaneKabz@users.noreply.github.com>
2020-03-09 09:07:34 +0100

    Update DatabaseHelper.java

--- main/java/com/example/belatarr/DatabaseHelper.java
+++ main/java/com/example/belatarr/DatabaseHelper.java
@@ -6,9 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by ProgrammingKnowledge on 4/3/2015.
- */
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Android.db";
     public static final String TABLE_NAME = "tasks";
